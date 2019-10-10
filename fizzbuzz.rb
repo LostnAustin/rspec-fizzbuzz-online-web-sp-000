bdfef3a391932b4719a7783a71cebd7e87ab216f
Chad Alan Lengefeld <chadlengefeld@gmail.com>
2019-10-10 18:51:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
 
-  if int % 3 == 0 && i % 5 == 0
+  if (int % 3 == 0) && (int % 5 == 0)
     "fizzbuzz"
 
   elsif int % 3 == 0
   "Fizz"
 
-else number % 5 == 0
+else int % 5 == 0
     "Buzz"
     end
   end
