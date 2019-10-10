686614cfbaff82a3d1d0be6bb3d12ba7cb319a27
Chad Alan Lengefeld <chadlengefeld@gmail.com>
2019-10-10 18:36:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
   "Fizz"
-else int % 5 == 0
+  if int % 5 == 0
   "Buzz"
-else ((int % 3) == 0) &&
-else ((int % 5) == 0)
+ if ((int % 3) == 0) &&
+ ((int % 5) == 0)
   "Fizzbuzz"
   end
 end
